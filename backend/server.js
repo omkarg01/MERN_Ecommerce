811e354e4e99e11423f34e1f626d3d972269c86c
omkarg01 <omkargujja01@gmail.com>
2022-01-20 15:23:47 +0530

    Prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -33,6 +33,7 @@ app.get('/api/config/paypal', (req, res) =>
 )
 
 const __dirname = path.resolve()
+console.log(__dirname);
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 if (process.env.NODE_ENV === 'production') {
