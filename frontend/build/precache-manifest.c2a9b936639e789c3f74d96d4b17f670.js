d8fe0b9bedfd5166a1f0b8df4c1e4d4eddeaf419
omkarg01 <omkargujja01@gmail.com>
2022-12-01 17:54:18 +0530

    build added

---
+++ frontend/build/precache-manifest.c2a9b936639e789c3f74d96d4b17f670.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4eff590304b5acbc0b026bb23688536a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b951a316a8d5a5c5168d",
+    "url": "/static/css/main.069c3d0d.chunk.css"
+  },
+  {
+    "revision": "62d3f766b0cee63b734c",
+    "url": "/static/js/2.022ac58d.chunk.js"
+  },
+  {
+    "revision": "c21d579849576bf6a47550572ff70f62",
+    "url": "/static/js/2.022ac58d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b951a316a8d5a5c5168d",
+    "url": "/static/js/main.808f5c9d.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.808f5c9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b6b46ef1e17d32e6df4",
+    "url": "/static/js/runtime-main.bce919bb.js"
+  }
+]);
